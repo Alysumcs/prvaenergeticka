c3238c3dfce350de398372c583f3af3eccbf2ec8
Alysumcs <d.pavelka77@gmail.com>
2026-06-16 10:31:12 +0200

    Delete templates directory

--- templates/404.php
+++
@@ -1,8 +0,0 @@
-<section class="section" style="min-height:60vh;display:flex;align-items:center;">
-  <div class="container section-head">
-    <h1 class="reveal" style="font-size:5rem;color:var(--accent);">404</h1>
-    <h2 class="reveal">Stránka sa nenašla</h2>
-    <p class="lead reveal">Ľutujeme, hľadaná stránka neexistuje alebo bola presunutá.</p>
-    <p class="reveal"><a class="btn btn-accent btn-lg" href="<?= e(url('')) ?>">Späť na domov</a></p>
-  </div>
-</section>
