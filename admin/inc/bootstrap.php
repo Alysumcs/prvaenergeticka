a99f603ae783ba5dc826ddf865c264266794a92b
Alysumcs <d.pavelka77@gmail.com>
2026-06-16 10:30:25 +0200

    Delete admin directory

--- admin/inc/bootstrap.php
+++
@@ -1,4 +0,0 @@
-<?php
-require __DIR__ . '/../../config.php';
-require __DIR__ . '/../../includes/db.php';
-require __DIR__ . '/../../includes/helpers.php';
--- admin/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require __DIR__ . '/inc/bootstrap.php';
-$_SESSION = [];
-session_destroy();
-redirect('admin/login.php');
